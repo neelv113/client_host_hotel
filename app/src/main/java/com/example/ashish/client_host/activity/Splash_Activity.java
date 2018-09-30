62d29fa5fd12c044b0b48f306eda50ea7889d55a
ashishbkha <ashishkha1000@gmail.com>
2018-09-30 11:03:19 +0530

    splash

---
+++ app/src/main/java/com/example/ashish/client_host/activity/Splash_Activity.java
@@ -0,0 +1,27 @@
+package com.example.ashish.client_host.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ashish.pre_booked_hotel.R;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_Activity.this,FoodMenuActivity.class));
+                finish();
+            }
+        },5000);
+    }
+}
