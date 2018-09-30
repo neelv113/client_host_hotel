e9677187a887b6c453b8135e0abb510f31cdd72a
LAPTOP-U27NE8GV\Neel <vyasneel123@gmail.com>
2018-09-30 11:41:04 +0530

    activity files

--- app/src/main/java/com/example/ashish/client_host/activity/DrinkOrderActivity.java
+++ app/src/main/java/com/example/ashish/client_host/activity/DrinkOrderActivity.java
@@ -1,4 +1,15 @@
 package com.example.ashish.client_host.activity;
 
-public class DrinkOrderActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ashish.pre_booked_hotel.R;
+
+public class DrinkOrderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_order);
+    }
 }
