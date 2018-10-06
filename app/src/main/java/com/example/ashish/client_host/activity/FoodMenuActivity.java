49c205587a3bf0a98eb89fcc9a2ee4a7e04c1125
LAPTOP-U27NE8GV\Neel <vyasneel123@gmail.com>
2018-10-06 18:42:44 +0530

    food and order button event done by nv

--- app/src/main/java/com/example/ashish/client_host/activity/FoodMenuActivity.java
+++ app/src/main/java/com/example/ashish/client_host/activity/FoodMenuActivity.java
@@ -1,15 +1,36 @@
 package com.example.ashish.client_host.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ashish.pre_booked_hotel.R;
 
 public class FoodMenuActivity extends AppCompatActivity {
+    private Button btn1,btn2;
     @Override
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_menu);
+
+        btn1 = (Button)findViewById(R.id.btnFood);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FoodMenuActivity.this,FoodOrderActivity.class).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+        btn2 = (Button)findViewById(R.id.btnDrinks);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FoodMenuActivity.this,DrinkOrderActivity.class).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 }
