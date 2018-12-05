9239e9910fdf6e7caa504089b02eef2d58aa5e71
LAPTOP-U27NE8GV\Neel <vyasneel123@gmail.com>
2018-12-05 20:28:41 +0530

    till drink button recycler view

--- app/src/main/java/com/example/ashish/client_host/Adapter/RecyclerDrinkAdapter.java
+++ app/src/main/java/com/example/ashish/client_host/Adapter/RecyclerDrinkAdapter.java
@@ -12,23 +12,23 @@ import android.widget.Toast;
 
 import com.example.ashish.pre_booked_hotel.R;
 
-public class RecyclerVegViewAdapter extends RecyclerView.Adapter<RecyclerVegViewAdapter.ViewHolder> {
-    private static final String TAG = RecyclerVegViewAdapter.class.getSimpleName();
+public class RecyclerDrinkAdapter extends RecyclerView.Adapter<RecyclerDrinkAdapter.ViewHolder> {
+    private static final String TAG = RecyclerDrinkAdapter.class.getSimpleName();
     private Context context;
 
-    public RecyclerVegViewAdapter(Context context){
-        this.context = context;
+    public RecyclerDrinkAdapter(Context context)
+    {
+        this.context=context;
     }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view, parent, false);
         return new ViewHolder(view);
-
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int  position) {
         holder.setData(position);
         holder.txtView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +36,7 @@ public class RecyclerVegViewAdapter extends RecyclerView.Adapter<RecyclerVegView
                 Toast.makeText(context,""+position,Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
     @Override
@@ -43,7 +44,7 @@ public class RecyclerVegViewAdapter extends RecyclerView.Adapter<RecyclerVegView
         return 15;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView txtView;
 
         public ViewHolder(View itemView) {
@@ -56,5 +57,4 @@ public class RecyclerVegViewAdapter extends RecyclerView.Adapter<RecyclerVegView
             txtView.setText("" + position);
         }
     }
-    }
-
+}
--- app/src/main/java/com/example/ashish/client_host/activity/DrinkOrderActivity.java
+++ app/src/main/java/com/example/ashish/client_host/activity/DrinkOrderActivity.java
@@ -1,15 +1,32 @@
 package com.example.ashish.client_host.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+
+
+import com.example.ashish.client_host.Adapter.RecyclerDrinkAdapter;
 import com.example.ashish.pre_booked_hotel.R;
 
 public class DrinkOrderActivity extends AppCompatActivity {
+    private RecyclerView recyclerDrink;
+    private RecyclerDrinkAdapter recyclerDrinkAdapter;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_order);
+        initViews();
     }
+
+    private void initViews() {
+        recyclerDrink = findViewById(R.id.recyclerDrink);
+        recyclerDrink.setLayoutManager(new LinearLayoutManager(this));
+        recyclerDrinkAdapter = new RecyclerDrinkAdapter(this);
+        recyclerDrink.setAdapter(recyclerDrinkAdapter);
+    }
+
+
 }
+
