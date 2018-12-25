979824df7ac0d454b9d766535250ee501ab732d5
ashishbkha <ashishkha1000@gmail.com>
2018-12-25 19:39:31 +0530

    order tab

--- app/src/main/java/com/example/ashish/client_host/activity/Splash_Activity.java
+++ app/src/main/java/com/example/ashish/client_host/activity/Splash_Activity.java
@@ -18,7 +18,7 @@ import com.example.ashish.pre_booked_hotel.R;
 public class Splash_Activity extends AppCompatActivity {
     private static final String TAG =Splash_Activity.class.getSimpleName() ;
     private Activity context;
-    String fixssid="\"Vyas\"";
+    String fixssid="\"No internet\"";
 
 
     protected void onCreate(@Nullable Bundle savedInstance)
