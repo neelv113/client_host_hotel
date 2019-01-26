c9b09561ae1729fd017c700113d23ed6cc5cb6b0
LAPTOP-U27NE8GV\Neel <vyasneel123@gmail.com>
2019-01-26 13:04:18 +0530

    Rating

--- app/src/main/java/com/example/ashish/client_host/activity/ToolbarRatingActivity.java
+++ app/src/main/java/com/example/ashish/client_host/activity/ToolbarRatingActivity.java
@@ -2,15 +2,30 @@ package com.example.ashish.client_host.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.ashish.pre_booked_hotel.R;
 
 public class ToolbarRatingActivity extends AppCompatActivity {
 
+    RatingBar ratingBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar_rating);
+
+        ratingBar = findViewById(R.id.Rating);
+        textView = findViewById(R.id.RatingText);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean b)
+            {
+                textView.setText("Value is : "+rating);
+            }
+        });
     }
 }
